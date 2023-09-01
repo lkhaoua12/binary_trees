35ea76e76eecdc8a4edc73ab451a9491dec67b48
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-01 07:06:14 +0100

    fix memory leak

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
-
-
+/**
+ * binary_tree_is_leaf - Check if a node is a leaf
+ * @tree: Pointer to node to check if leaf.
+ *
+ * Return: 1 if true, 0 otherwise.
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL || node->left || node->right)
