aa868a624e83323a6a06c3163c774490c0029b92
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-09-01 07:00:01 +0100

    fix memory leak

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,6 @@
 #ifndef B_TREE
 #define B_TREE
-
+#include "stdlib.h"
 #include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
@@ -26,4 +26,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 #endif
